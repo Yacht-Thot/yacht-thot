802d82ffc7440cf7e5042dc969e45b9d6fa569cd
Nnicholas Conrad <nicholasconrad96@gmail.com>
2024-07-09 06:09:57 -0400

    WinLap commit

--- public/js/core.js
+++ public/js/core.js
@@ -6,15 +6,69 @@ document.getElementById("mobile-menu").onclick = function() {
 }
 
 if(document.getElementById("thot-profile-like")) {
+    /** LIKE CIRCLE
     document.getElementById("thot-profile-like").onclick = function() {
         document.getElementById("thot-profile-like-icon").classList.toggle("fa-solid")
         document.getElementById("thot-profile-like-icon").classList.toggle("fa-regular")
         document.getElementById("thot-profile-like-icon").classList.toggle("green-icon")
-    }
+    }**/
     document.getElementById("thot-profile-back").onclick = function() {
-        window.location.href = "/captain-feed"
+        window.location.href = "/feed"
     }
 }
+
+
+function likeProfile(user_id) {
+    alert("Liking " + user_id)
+}
+
+window.likeProfile = likeProfile;
+
+var messages_body = document.getElementById("messages-body")
+
+function sendIM(message) {
+    //alert(message)
+    document.getElementById("messages-textbox").value = ""
+
+    var my_message = document.createElement("div")
+    my_message.classList.add("my-message")
+    my_message.innerHTML = message;
+    messages_body.appendChild(my_message)
+    messages_body.scrollTop = messages_body.scrollHeight;
+
+
+    var payload = {
+        message_content: message,
+        to_uid: document.getElementById("messages-textbox").dataset.uid
+    }
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+           console.log(this.responseText)
+
+        }
+    }
+    xhttp.open("POST", "/send-private-message", true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify( payload ));
+
+
+}
+function checkMessage(e) {
+    var key = e.keyCode;
+
+    // If the user has pressed enter
+    if (key === 13) {
+        sendIM(document.getElementById("messages-textbox").value);
+        e.preventDefault();
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+window.checkMessage = checkMessage;
 
 
 
@@ -36,6 +90,7 @@ function submitOnboarding() {
     var payload = {
         role: role,
         username: document.getElementById("thot-username").value,
+        location: document.getElementById("thot-location").value,
         birthday: document.getElementById("thot-birthday").value,
         tagline: document.getElementById("thot-tagline").value,
         bio: document.getElementById("thot-bio").value
@@ -62,7 +117,7 @@ function submitOnboarding() {
         if (this.readyState == 4 && this.status == 200) {
            console.log(this.responseText)
 
-            window.location.href = "/profile"
+            window.location.href = "/my-profile"
         }
     }
     xhttp.open("POST", "/submit-onboarding", true);
@@ -86,6 +141,16 @@ var test_data = [
 
 function showMessagesInterface() {
 
+}
+
+
+//////////////////////////START///////////////////////////////
+
+if(document.getElementById("profile-image-edit")) {
+    document.getElementById("profile-image-edit").onclick = function() {
+        console.log("edit");
+        
+    }
 }
 
 if(window.location.href.includes("/my-profile")) {
@@ -122,3 +187,7 @@ if(document.getElementById("messages-ab-icon")) {
         window.location.href = "/messages"
     }
 }
+
+if(user_data.username == null && !window.location.href.includes("/onboarding") && user_data != -1) {
+    window.location.href = "/onboarding"
+ }
